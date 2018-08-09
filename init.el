a04a6fb28b2ff09cf1abd9434b893a8bc15e0fc5
mason <mason@localhost.localdomain>
2018-08-09 16:59:48 +0800

    修改返回绑定键

--- init.el
+++ init.el
@@ -30,7 +30,7 @@
   (add-hook 'before-save-hook 'gofmt-before-save)
   ; Godef jump key binding                                                      
   (local-set-key (kbd "M-.") 'godef-jump)
-  (local-set-key (kbd "M-*") 'pop-tag-mark)
+  (local-set-key (kbd "M-,") 'pop-tag-mark)
   )
 
 (defun auto-complete-for-go ()
