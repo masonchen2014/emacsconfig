943d120e66379294835721fdda21a8ff67a4a6ef
mason <mason@localhost.localdomain>
2018-08-16 21:49:58 +0800

    add c/c++ support

--- init.el
+++ init.el
@@ -52,3 +52,32 @@
 
 ;;invoke load-go-mode when open a go file
 (add-to-list 'auto-mode-alist '("\\.go\\'" . load-go-mode))
+
+
+
+
+;;for c/c++ mode
+(defun load-c-c++-config ()
+  (progn
+    (load-package-hard 'setup-helm)
+    (load-package-hard 'helm-gtags)
+    (setq
+     helm-gtags-ignore-case t
+     helm-gtags-auto-update t
+     helm-gtags-use-input-at-cursor t
+     helm-gtags-pulse-at-cursor t
+     helm-gtags-prefix-key "\C-cg"
+     helm-gtags-suggested-key-mapping t
+     )
+    (define-key helm-gtags-mode-map (kbd "C-c g a") 'helm-gtags-tags-in-this-function)
+    (define-key helm-gtags-mode-map (kbd "C-j") 'helm-gtags-select)
+    (define-key helm-gtags-mode-map (kbd "M-.") 'helm-gtags-dwim)
+    (define-key helm-gtags-mode-map (kbd "M-,") 'helm-gtags-pop-stack)
+    (define-key helm-gtags-mode-map (kbd "C-c <") 'helm-gtags-previous-history)
+    (define-key helm-gtags-mode-map (kbd "C-c >") 'helm-gtags-next-history)
+
+     (helm-gtags-mode)
+))
+
+(add-hook 'c-mode-hook 'load-c-c++-config)
+(add-hook 'c++-mode-hook 'load-c-c++-config)
